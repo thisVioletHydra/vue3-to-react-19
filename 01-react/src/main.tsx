f2b7328d1eda8d16575cde176308a8318e2d76b5
thisVioletHydra <thisVioletHydra@gmail.com>
2026-08-21 14:37:21 +0600

    checkpoint before checking out master

--- 01-react/src/main.tsx
+++ 01-react/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+const router = createBrowserRouter([{}]);
+
 createRoot(document.querySelector("#app")!).render(
   <StrictMode>
     <App></App>
